9bc564d7da48a0a13617bd5829f910dde4e432b2
Hiba <hiba.allache@gmail.com>
2023-02-16 14:45:25 -0800

    prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error. use write instead of printf and puts. program should return 1 .

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <unistd.h>
 /**
  * main - Starting the program
- * We wrote 58 because we want to print all the string. (58 is the length of the string)
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
