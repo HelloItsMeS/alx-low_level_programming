2317e8cf368f6e4014d6d3b7878c5ce5e9bed502
Hiba <hiba.allache@gmail.com>
2023-02-16 13:50:05 -0800

    prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line. Use the function printf. program should compile without warning when using the -Wall gcc option. program should return 0

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program that Use the function printf to print a string
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,")
+	return (0);
+}
