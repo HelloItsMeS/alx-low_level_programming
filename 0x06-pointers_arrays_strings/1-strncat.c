9b7ac5632fd4fc6737560d40b759a2e5d9f5f242
Hiba <hiba.allache@gmail.com>
2023-03-02 14:14:52 -0800

    Write a function that concatenates two strings.
        Prototype: char *_strncat(char *dest, char *src, int n);
        The _strncat function is similar to the _strcat function, except that
            it will use at most n bytes from src; and
            src does not need to be null-terminated if it contains n or more bytes
        Return a pointer to the resulting string dest
    
    FYI: The standard library provides a similar function: strncat. Run man strncat to learn more.

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,28 +8,19 @@
  * @n: input value
  * Return: dest
  */
-char *_strcat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-	int len1; /* string length for src */
-	int len2; /* string length for dest */
-	int i;
+	int dest_length = _strlen(dest), i = 0;
 
-	len1 = 0;
-	len2 = 0;
-
-	while (src[len1] != '\0')
-		len1++;
-
-	while (dest[len2] != '\0')
-		len2++;
-
-	if (n <= len1)
+	while (i < n && *(src + i) != '\0')
 	{
-	for (i = 0; i <= n; i++)
-	{
-		dest[len2] = src[i];
-		len2++;
+		*(dest + dest_length) = *(src + i);
+
+		i++;
+		dest_length++;
 	}
-	}
+
+	*(dest + dest_length) = '\0';
+
 	return (dest);
 }
